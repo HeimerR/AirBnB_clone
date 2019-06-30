b51b0a9b4850bed5217d9203fb2cf773a7ec5e67
Heimer <732@holbertonschool.com>
2019-06-30 22:12:32 +0000

    [feature] comments added to basemodel and console

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-"""base model"""
+"""
+Parent Module -  all common attributes/methods for other classes
+
+"""
 from uuid import uuid4
 from datetime import datetime
 import models
 
 
 class BaseModel:
-    """ class base model """
+    """ Parent class - Encharge of serialization/deserialization process
+        Add an unique ID for every instace
+        Add time for creating and updating
+
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Set inital values for every intance
+        Args:
+            *args: Is not used
+            **kwargs: instance attributes - each key is an attribute name
+
+        """
         if (kwargs):
                 for key, value in kwargs.items():
                     if key != "__class__":
@@ -25,14 +39,34 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
+        """
+        Represetation of a intaces
+
+        Returns:
+            str: [<class name>] (<self.id>) <self.__dict__>
+
+        """
         cls = type(self).__name__
         return "[{}] ({}) {}".format(cls, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates time and save changes into __objects (in FileStorage)
+
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """
+        dictionary representation fo every intance
+        time format: %Y-%m-%dT%H:%M:%S.%f
+        key __class__ added to identify every intance
+
+        Returns:
+            dict: dictionary
+
+        """
         dictionary = self.__dict__.copy()
         dictionary.update({'__class__': type(self).__name__})
         dictionary['created_at'] = self.created_at.isoformat()
