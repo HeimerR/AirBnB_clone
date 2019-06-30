6bb8d9193f3650ea629f330deb13324006f94db9
Haroldo V <hvelez020@gmail.com>
2019-06-30 18:27:57 +0000

    [add] state, city, place, amenity, review classes

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" doc """
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ doc """
+    
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" doc """
+from base_model import BaseModel
+
+
+class State(BaseModel):
+    """ doc """
+    
+    name = ""
+
+
+
