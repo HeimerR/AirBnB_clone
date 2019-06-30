6c2ff3e512b6f33b9da52bb4dc684e5e6614d0ea
Heimer <732@holbertonschool.com>
2019-06-30 18:08:36 +0000

    [fix] update command

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+import shlex
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -74,14 +76,14 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_update(self, line):
-        list_arg = line.split(" ")
+        list_arg = shlex.split(line)
         if list_arg[0] == "":
             print("** class name missing **")
-        elif (list_arg[0] != "BaseModel"):
+        elif  not list_arg[0] in HBNBCommand.name_classes:
             print("** class doesn't exist **")
         elif len(list_arg) == 1:
             print("** instance id missing **")
-        elif "BaseModel.{}".format(list_arg[1]) not in storage.all().keys():
+        elif "{}.{}".format(list_arg[0], list_arg[1]) not in storage.all().keys():
             print("** no instance found **")
         elif len(list_arg) == 2:
             print("** attribute name missing **")
@@ -89,7 +91,7 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing **")
         else:
             dict_objs = storage.all()
-            aux = "BaseModel.{}".format(list_arg[1])
+            aux = "{}.{}".format(list_arg[0], list_arg[1])
             if aux in dict_objs.keys():
                 attr = getattr(dict_objs[aux], list_arg[2], "")
                 setattr(dict_objs[aux], list_arg[2], type(attr)(list_arg[3]))
