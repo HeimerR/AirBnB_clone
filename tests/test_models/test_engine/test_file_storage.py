8e4accdbaa24b1abcdf45752308a3f4611e10333
Heimer <732@holbertonschool.com>
2019-07-02 22:26:55 +0000

    [feature] test for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,67 +2,67 @@
 """ doctest unittest """
 import unittest
 import pep8
+from models.base_model import BaseModel
+from models.base_model.engine import FileStorage
+from models.__init__ import storage
+import os
 
-class Testpep8(unittest.TestCase):
-    def test_pep8(self):
-        style = pep8.StyleGuide(quiet=True)
-        file_base_model = "models/base_model.py"
-        check = style.check_files([file_base_model])
-        self.assertEqual(check.total_errors, 0,
-                         "Found code style errors (and warning).")
 
 class TestBase(unittest.TestCase):
     """ test """
 
+    def test_pep8_filestorage(self):
+        """ test pep8 """
+        style = pep8.StyleGuide(quiet=True)
+        file_fs = "models/engine/file_storage.py"
+        file_test_fs = "tests/test_models/test_engine/test_file_storage.py"
+        check = style.check_files([file_fs, file_test_fs])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warning).")
+
     @classmethod
     def setUpClass(cls):
-        tmp = style.check_files(["models/base_model.py"])
+        """ first set up
+        check = style.check_files([file_base_model, file_test_base_model])
+        """
+        cls.ins = FileStorage()
 
     @classmethod
-    def tearDownClass(cls):
-        pass
+    def teardown(cls):
+        """ final statement """
+        del cls.ins
+        try:
+            os.remove("file.json")
+        except:
+            pass
 
-    def setUp(self):
-        pass
+    def test_FileStorage_methods(self):
+        """ test base model documentation
+        self.assertNotEqual(len(models.__doc__), 0)
+        self.assertNotEqual(len(models.base_model.__doc__), 0)
 
-    def tearDown(self):
-        pass
+        """
+        self.assertNotEqual(len(FileStorage.__doc__), 0)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
 
-    """ def test_setid(self):
-        self.assertEqual(self.b1.id, 7)
+    def test_FileStorsageAttr(self):
+        """ test basemodel attributes """
+        self.assertEqual(hasattr(self.ins, "_FileStorage__file_path"), True)
+        self.assertEqual(hasattr(self.ins, "_FileStorage__objects"), True)
 
-    def test_setid2(self):
-        self.assertEqual(self.b2.id, 11)
+    def test_isinstanceofFileStorage(self):
+        self.assertTrue(isinstance(self.ins, FileStorage))
 
-    def test_setid3(self):
-        self.assertEqual(self.b3.id, -1)
+    def test_allFS(self):
+        my_dict = self.ins.all()
+        self.assertTrue(type(my_dict) is dict)
 
-    def test_setid4(self):
-        self.assertEqual(self.b4.id, 12)
-
-    def test_setid5(self):
-        self.assertEqual(self.b5.id, "hola")
-
-    def test_setid6(self):
-        self.assertEqual(self.b6.id, 3.1)
-
-    def test_setid7(self):
-        self.assertEqual(self.b7.id, 27)
-
-    def test_json_string(self):
-        r1 = Rectangle(10, 7, 2, 8, 1)
-        dictionary = r1.to_dictionary()
-        json_dictionary = Base.to_json_string([dictionary])
-        self.assertEqual(type(json_dictionary), str)
-
-    def test_json_string2(self):
-        r1 = Rectangle(10, 7, 2, 8, 1)
-        dictionary = r1.to_dictionary()
-        json_dictionary = Base.to_json_string([dictionary])
-        list_back = json.loads(json_dictionary)
-        self.assertEqual(list_back, [dictionary])
-    """
+    def test_saveFS(self):
+        self.ins.save()
+        self.assertTrue(os.path.exists('file.json'))
 
 if __name__ == '__main__':
     unittest.main()
-
