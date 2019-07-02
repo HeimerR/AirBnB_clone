c96696c681ceebec424d514e7e7b06b7cb72e02e
Heimer <732@holbertonschool.com>
2019-07-02 23:17:04 +0000

    [feature] test place added

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,67 +2,62 @@
 """ doctest unittest """
 import unittest
 import pep8
+from models.place import Place
+import os
 
-class Testpep8(unittest.TestCase):
-    def test_pep8(self):
-        style = pep8.StyleGuide(quiet=True)
-        file_base_model = "models/base_model.py"
-        check = style.check_files([file_base_model])
-        self.assertEqual(check.total_errors, 0,
-                         "Found code style errors (and warning).")
 
 class TestBase(unittest.TestCase):
     """ test """
 
+    def test_pep8(self):
+        """ test pep8 """
+        style = pep8.StyleGuide(quiet=True)
+        file_place = "models/place.py"
+        file_test_place = "tests/test_models/test_place.py"
+        check = style.check_files([file_place, file_test_place])
+        self.assertEqual(check.total_errors, 0,
+                         "Found code style errors (and warning).")
+
     @classmethod
     def setUpClass(cls):
-        pass
+        """ first set up """
+        check = style.check_files([file_place, file_test_place])
+
+        cls.ins = Place()
 
     @classmethod
-    def tearDownClass(cls):
-        pass
+    def teardown(cls):
+        """ final statement """
+        del cls.ins
+        try:
+            os.remove("file.json")
+        except:
+            pass
 
-    def setUp(self):
-        pass
+    def test_Userdoc(self):
+        """ test base model documentation
+        self.assertNotEqual(len(models.__doc__), 0)
+        self.assertNotEqual(len(models.base_model.__doc__), 0)
 
-    def tearDown(self):
-        pass
+        """
+        self.assertNotEqual(len(Place.__doc__), 0)
 
-    """ def test_setid(self):
-        self.assertEqual(self.b1.id, 7)
+    def test_BaseModelAttr(self):
+        """ test basemodel attributes """
+        self.assertEqual(hasattr(self.ins, "city_id"), True)
+        self.assertEqual(hasattr(self.ins, "user_id"), True)
+        self.assertEqual(hasattr(self.ins, "name"), True)
+        self.assertEqual(hasattr(self.ins, "description"), True)
+        self.assertEqual(hasattr(self.ins, "number_rooms"), True)
+        self.assertEqual(hasattr(self.ins, "number_bathrooms"), True)
+        self.assertEqual(hasattr(self.ins, "max_guest"), True)
+        self.assertEqual(hasattr(self.ins, "price_by_night"), True)
+        self.assertEqual(hasattr(self.ins, "latitude"), True)
+        self.assertEqual(hasattr(self.ins, "longitude"), True)
+        self.assertEqual(hasattr(self.ins, "amenity_ids"), True)
 
-    def test_setid2(self):
-        self.assertEqual(self.b2.id, 11)
-
-    def test_setid3(self):
-        self.assertEqual(self.b3.id, -1)
-
-    def test_setid4(self):
-        self.assertEqual(self.b4.id, 12)
-
-    def test_setid5(self):
-        self.assertEqual(self.b5.id, "hola")
-
-    def test_setid6(self):
-        self.assertEqual(self.b6.id, 3.1)
-
-    def test_setid7(self):
-        self.assertEqual(self.b7.id, 27)
-
-    def test_json_string(self):
-        r1 = Rectangle(10, 7, 2, 8, 1)
-        dictionary = r1.to_dictionary()
-        json_dictionary = Base.to_json_string([dictionary])
-        self.assertEqual(type(json_dictionary), str)
-
-    def test_json_string2(self):
-        r1 = Rectangle(10, 7, 2, 8, 1)
-        dictionary = r1.to_dictionary()
-        json_dictionary = Base.to_json_string([dictionary])
-        list_back = json.loads(json_dictionary)
-        self.assertEqual(list_back, [dictionary])
-    """
+    def test_isinstance(self):
+        self.assertTrue(isinstance(self.ins, Place))
 
 if __name__ == '__main__':
     unittest.main()
-
