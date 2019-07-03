cd9f07b3fedb59d7b20566965fd4e52290da823f
Heimer <732@holbertonschool.com>
2019-07-03 01:28:46 +0000

    [feature] test save function in basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -60,6 +60,15 @@ class TestBase(unittest.TestCase):
     def test_save_updated_at_created_at(self):
         self.ins.save()
         self.assertNotEqual(self.ins.created_at, self.ins.updated_at)
+        dummy = BaseModel()
+        my_id = dummy.id
+        dummy.name = "Haroldo"
+        dummy.save()
+        storage.reload()
+        my_objs = storage.all()["BaseModel.{}".format(my_id)]
+        self.assertTrue(hasattr(my_objs, "name"))
+        self.assertTrue(my_objs.name == "Haroldo")
+        self.assertTrue(os.path.exists('file.json'))
 
     def test_dict(self):
         dicto = self.ins.to_dict()
